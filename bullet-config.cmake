8a783e1391cbe46f295a45bc22bdb502e8d43657
cj <cj@silentorb.com>
2017-11-18 13:38:39 -0700

    Installation is now working.

--- bullet-config.cmake
+++ bullet-config.cmake
@@ -1,11 +1,29 @@
 
 set(lib_dir "${CMAKE_CURRENT_LIST_DIR}/bin/${GENERATOR_KEY}/lib")
 
+list(APPEND libraries
+  #  Bullet2FileLoader
+  Bullet3Collision
+  Bullet3Common
+  Bullet3Dynamics
+  Bullet3Geometry
+  #  Bullet3OpenCL_clew
+  BulletCollision
+  BulletDynamics
+  BulletInverseDynamics
+  BulletSoftBody
+  LinearMath
+  )
+
 if (MINGW)
-  target_link_libraries(${CURRENT_TARGET} "${lib_dir}/libbullet.a")
+  foreach (lib IN libraries)
+    target_link_libraries(${CURRENT_TARGET} "${lib_dir}/${lib}.a")
+  endforeach ()
 elseif (MSVC)
-  target_link_libraries(${CURRENT_TARGET} "${lib_dir}/libbullet.lib")
+  set(DEBUG_SUFFIX _Debug)
+  target_link_libraries(${CURRENT_TARGET} "${lib_dir}/${lib}${DEBUG_SUFFIX}.lib")
 else ()
   message(FATAL_ERROR "Platform support not yet implemented.")
 endif ()
+
 include_directories(${CMAKE_CURRENT_LIST_DIR}/bin/${GENERATOR_KEY}/include)
